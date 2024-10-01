874c1cd4e091e5edc30160500c7abc722a8a4fe6
forgottentyranny <forgottentyranny@gmail.com>
2024-10-01 16:56:26 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -4,11 +4,16 @@ let Header = document.createElement("div");
 Header.classList.add("main_items");
 Header.classList.add("header");
 MainContainer.appendChild(Header);
+let ProfilePictureWText = document.createElement("div");
+ProfilePictureWText.classList.add("header_items");
+Header.appendChild(ProfilePictureWText)
 let ProfilePicture = document.createElement("img");
-ProfilePicture.classList.add("header_items");
 ProfilePicture.classList.add("profile_picture");
 ProfilePicture.src = "..\\images\\profile_picture.png";
 ProfilePicture.height = 200;
 ProfilePicture.width = 200;
-Header.appendChild(ProfilePicture);
+ProfilePictureWText.appendChild(ProfilePicture);
+let ProfileText = document.createElement("p");
+ProfileText.innerHTML = "Profile Picture Placeholder";
+ProfilePictureWText.appendChild(ProfileText);
 document.body.appendChild(MainContainer);
